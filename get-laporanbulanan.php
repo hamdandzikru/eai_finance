807b95587268d5bc50e6069b5dc84b5a7783d170
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-29 22:53:26 +0700

    completing get

--- get-laporanbulanan.php
+++ get-laporanbulanan.php
@@ -5,7 +5,16 @@
     $sql = "SELECT * FROM laporan_bulanan";
     $query = mysqli_query($con, $sql);
     while($data = mysqli_fetch_array($query)){
-        echo $data["id_laporanbulan"]." ";
+        // echo $data["id_laporanbulan"]." ";
+
+        $item[] = array(
+            'id_laporanbulanan'=>$data["id_laporanbulan"],
+            'id_divisi'=>$data["id_divisi"],
+            'bulan'=>$data["bulan"],
+            'totaltransaksi'=>$data["totaltransaksi"],
+            'income'=>$data["income"],
+            'outcome'=>$data["outcome"]
+        )
     }
 
 ?>
