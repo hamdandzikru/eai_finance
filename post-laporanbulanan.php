e347e01074812d2d46f589052379d3ae2505a35b
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-29 22:22:22 +0700

    updated

--- post-laporanbulanan.php
+++ post-laporanbulanan.php
@@ -3,21 +3,26 @@ include "conn.php";
 	
 	$id_laporanbulan = isset($_POST["id_laporanbulan"]) ? $_POST["id_laporanbulan"] : "";
 	echo $id_laporanbulan;
+
 	$id_divisi = isset($_POST["id_divisi"]) ? $_POST["id_divisi"] : "";
 	echo $id_divisi;
+
 	$bulan = isset($_POST["bulan"]) ? $_POST["bulan"] : "";
 	echo $bulan;
+
 	$totaltransaksi = isset($_POST["totaltransaksi"]) ? $_POST["totaltransaksi"] : "";
 	echo $totaltransaksi;
+
 	$income = isset($_POST["income"]) ? $_POST["income"] : "";
 	echo $income;
+
 	$outcome = isset($_POST["outcome"]) ? $_POST["outcome"] : "";
 	echo $outcome;
 
 	$sql = "INSERT INTO `laporan_bulanan` (`id_laporanbulan`, `id_divisi`, `bulan`, `totaltransaksi`, `income`, `outcome`) 
-	VALUES ('11987', '028', 'April', '298', '32000000', '11000000');";
+	VALUES ('".$id_laporanbulan."', '".$id_divisi."', '".$bulan."', '".$totaltransaksi."', '".$income."', '".$outcome."');";
 
-$sql = "INSERT INTO `laporan_bulanan` (`id_laporanbulan`, `id_divisi`, `bulan`, `totaltransaksi`, `income`, `outcome`) 
-VALUES ('11987', '028', 'April', '298', '32000000', '11000000');";
+// $sql = "INSERT INTO `laporan_bulanan` (`id_laporanbulan`, `id_divisi`, `bulan`, `totaltransaksi`, `income`, `outcome`) 
+// VALUES ('11987', '028', 'April', '298', '32000000', '11000000');";
 	
 ?>
