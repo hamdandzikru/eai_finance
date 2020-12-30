30f38bf9dd23eef1b6685c0dafd2545f5bbc08f6
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-30 22:02:32 +0700

    update api get divisi

--- get-divisi.php
+++ get-divisi.php
@@ -1,3 +1,23 @@
 <?php
 
+    include "conn.php";
+
+    $sql = "SELECT * FROM divisi";
+    $query = mysqli_query($con, $sql);
+    while($data = mysqli_fetch_array($query)){
+        // echo $data["id_laporanbulan"]." ";
+
+        $item[] = array(
+            'id_divisi'=>$data["id_divisi"],
+            'nama_divisi'=>$data["nama_divisi"]
+        );
+    }
+
+    $response = array(
+        'status'=>'OK',
+        'data'=>$item
+    );
+
+    echo json_encode($response);
+
 ?>
