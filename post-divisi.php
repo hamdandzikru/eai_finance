5aef5d6742ea1b2de60635dedef7565da36594f6
hamdandzikru <hamdanrobbi@gmail.com>
2020-12-30 21:30:14 +0700

    added api post divisi

---
+++ post-divisi.php
@@ -0,0 +1,32 @@
+<?php
+include "conn.php";
+	
+	$id_divisi = isset($_POST["id_divisi"]) ? $_POST["id_divisi"] : "";
+	echo $id_divisi;
+
+	$nama_divisi = isset($_POST["nama_divisi"]) ? $_POST["nama_divisi"] : "";
+    echo $nama_divisi;
+    
+
+	$sql = "INSERT INTO `divisi` (`id_laporanbulan`, `id_divisi`, `bulan`, `totaltransaksi`, `income`, `outcome`) 
+	VALUES ('".$id_laporanbulan."', '".$id_divisi."', '".$bulan."', '".$totaltransaksi."', '".$income."', '".$outcome."');";
+
+	echo $sql;
+	
+	//query
+	$query = mysqli_query($con, $sql);
+	if($query){
+		$msg = "Succesfully saved data!";
+	}else{
+		$msg = "Failed to save the data!";
+	}
+
+	// echo $msg;
+
+	$response = array(
+		'status' => 'OK',
+		'msg' => $msg
+	);
+
+	echo json_encode($response);
+?>
